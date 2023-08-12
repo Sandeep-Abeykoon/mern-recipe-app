8fd2b0d558057872ad51e350813fcf8126751b80
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-13 02:48:34 +0530

    updated the code

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,4 @@
+import { config } from 'dotenv';
 import express from "express";
 import cors from "cors";              // cors is to set rules of communication between the frontend and the backend
 import mongoose from 'mongoose';
@@ -7,5 +8,10 @@ const port = 4000;
 
 app.use(express.json());
 app.use(cors());
+
+config({ path: '../.env'});
+const encodedPassword = encodeURIComponent(process.env.DB_PASSWORD);
+
+mongoose.connect(`mongodb+srv://recipeApp:${encodedPassword}@recipes.ac4tdjw.mongodb.net/?retryWrites=true&w=majority`);
 
 app.listen(port, () => console.log(`Server running at port ${port}`));
