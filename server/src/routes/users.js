315c58ba0ca5f7f9eff6023acb07c32feaa0e40c
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-17 15:05:59 +0530

    Update users.js

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -5,12 +5,49 @@ import { UserModel } from '../models/Users.js';
 
 const router = express.Router();
 
+// New User register function
 router.post("/register", async(req, res) => {
     const { username, password } = req.body;
+
+    // Checking if the username already available
     const user = await UserModel.findOne({username});
-    res.json(user);
+
+    if (user) {
+        return res.json({ message: "User already exists!"});
+    }
+
+    // Decrypting the user password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Adding the new user to the database
+    const newUser = new UserModel({ username, password: hashedPassword});
+    await newUser.save();
+
+    res.json({ message: "User registered sucessfully" });
 });
 
-router.post("/login")
+
+// User login function
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    // Checking if the username already available
+    const user = await UserModel.findOne({username});
+
+    if (!user) {
+        return res.json({ message: "User doesn't exist!"});
+    }
+
+    //Comparing the becrypted user entered password with the database saved password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid) {
+        return res.json({ message: "Username or Password is Incorrect"});
+    }
+
+    
+
+});
+
 
 export { router as userRouter};
