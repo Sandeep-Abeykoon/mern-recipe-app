b99f6719839d2145b4a97c2bc090581fd94231b2
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-17 20:06:18 +0530

    updated the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,20 @@ import {
 } 
 from "react-router-dom";
 
+import { Home } from './pages/home';
+import { Auth } from './pages/auth';
+import { CreateRecipe } from './pages/create-recipe';
+import { SavedRecipes } from './saved-recipes';
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={}/>
+          <Route path='/' element={ <Home/> }/>
+          <Route path='/auth' element={ <Auth/> }/>
+          <Route path='/create-recipe' element={ <CreateRecipe/> }/>
+          <Route path='/saved-recipes' element={ <SavedRecipes/> }/>
         </Routes>
       </Router>
     </div>
