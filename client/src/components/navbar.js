4c97f03e9fad978b45824224bb848a8fe596c308
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-18 10:13:30 +0530

    updated the css file

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,15 +1,16 @@
 import { Component } from "react";
 import { MenuData } from "./menuData";
+import "./navbarStyle.css"; 
 
-console.log(MenuData);
+console.log(MenuData); 
 class Navbar extends Component{
     render(){
         return(
             <nav className="NavbarItems">
-                <h1>
+                <h1 className="logo">
                     ReciPro <i className="fas fa-hamburger"></i>
                 </h1>
-                <ul>
+                <ul className="nav-menu">
                     {MenuData.map((item, index) => {
                         return(
                             <li key={index}>
