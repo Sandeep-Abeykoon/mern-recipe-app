dd0bb5f037a8a7cc2e0936cabf65bb2fee2c8a34
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-18 09:02:15 +0530

    Update menuData.js

--- client/src/components/menuData.js
+++ client/src/components/menuData.js
@@ -2,25 +2,30 @@ export const MenuData = [
     {
         title: "Home",
         url: "#",
-        cName: "nav-Links",
+        cName: "nav-links",
         icon: "fa-solid- fa-house-user"
     },
     {
         title: "Create Recipe",
         url: "#",
-        cName: "nav-Links",
+        cName: "nav-links",
         icon: "fa-light fa-kitchen-set"
     },
     {
         title: "Saved Recipes",
         url: "#",
-        cName: "nav-Links",
+        cName: "nav-links",
         icon: "fa-light fa-list"
     },
     {
         title: "Additional Page",
         url: "#",
-        cName: "nav-Links",
+        cName: "nav-links",
         icon: "fa fa-sign-in"
     },
-]
+    {
+        title: "Login/Signup",
+        url: "#",
+        cName: "nav-links-mobile",
+    },
+];
