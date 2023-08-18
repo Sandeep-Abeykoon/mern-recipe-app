2a1ac77cf709bd6c9896123ae05e1a21fa183fef
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-18 18:29:04 +0530

    Update signup.js

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,32 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import "./styles/auth.css";
 
 export const Signup = () => {
-  return (
-    <div className='signup template d-flex justify-content-center align-items-center vh-100 text-white'>
-        <div className='form-container p-5 rounded'>
-            <form>
-                <h3 className='text-center'>Sign Up</h3>
 
-                <div className='mb-2'>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" placeholder='Enter username' className='form-control'/>
-                </div>
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-                <div className='mb-2'>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder='Enter password' className='form-control'/>
-                </div>
+    return (
+        <div className='signup template d-flex justify-content-center align-items-center vh-100 text-white'>
+            <div className='form-container p-5 rounded'>
+                <form>
+                    <h3 className='text-center'>Sign Up</h3>
 
-                <div className='d-grid'>
-                    <button className='btn btn-primary'>Sign Up</button>
-                </div>
-                <p className='text-end mt-2'>
-                    Already Registered <Link to="/auth" className='ms-2'>Sign in</Link>
-                </p>
-            </form>
+                    <div className='mb-2'>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" placeholder='Enter username' className='form-control' id='username' onChange={(event) => setUsername(event.target.value)}/>
+                    </div>
+
+                    <div className='mb-2'>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" placeholder='Enter password' className='form-control' id='password' onChange={(event) => event.target.value}/>
+                    </div>
+
+                    <div className='d-grid'>
+                        <button className='btn btn-primary'>Sign Up</button>
+                    </div>
+                    <p className='text-end mt-2'>
+                        Already Registered <Link to="/auth" className='ms-2'>Sign in</Link>
+                    </p>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
