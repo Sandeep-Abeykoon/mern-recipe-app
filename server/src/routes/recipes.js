b6ea458347ab713953b9dac5dca05a78db634702
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-19 11:18:28 +0530

    Update recipes.js

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -4,4 +4,13 @@ import { RecipeModel } from "../models/Recipes.js";
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+    try {
+        const response = await RecipeModel.find({});
+        res.json()
+    } catch (error) {
+        res.json(error);
+    }
+})
+
 export { router as recipesRouter }
