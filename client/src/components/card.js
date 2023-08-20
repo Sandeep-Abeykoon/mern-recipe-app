4003b8ba83e047e699337734e12a7411fca4da72
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-20 22:00:43 +0530

    updated the code

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "./card.css";
 
 export const Card = (props) => {
   return (
     <div className='card'>
         <div className='card-body'>
             <img 
+                className='card-image'
                 src= {props.img}
                 alt= {props.alt}
              />
              <h2 className='card-title'>{props.title}</h2>
-             <p className='class-description'>{props.description}</p>
+
+             {props.description.split('<br/>').map((line, index) => (
+                <div key={index} className='card-description'>{line}</div>
+             ))}
+             
         </div>
         <button className='card-button'>View Recipe</button>
     </div>
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -25,13 +25,35 @@ export const Home = () => {
     return (
         <div className="container">
             <h2 className="page-heading">Home</h2>
-            <Card
-                img = "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80"
-                alt = "Recipe cooked"
-                title = "BBQ Chicken Pizza"
-                description =  "Make the dove with all the ingredients\
-                bake The pizza dove in the oven"
-            />
+            <div className="card-container">
+                <Card
+                    img = "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80"
+                    alt = "Recipe cooked"
+                    title = "BBQ Chicken Pizza"
+                    description =  "Make the dove with all the ingredients. <br/>bake The pizza dove in the oven" 
+                />
+
+                <Card
+                    img = "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80"
+                    alt = "Recipe cooked"
+                    title = "BBQ Chicken Pizza"
+                    description =  "Make the dove with all the ingredients. <br/>bake The pizza dove in the oven" 
+                />
+
+                <Card
+                    img = "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80"
+                    alt = "Recipe cooked"
+                    title = "BBQ Chicken Pizza"
+                    description =  "Make the dove with all the ingredients. <br/>bake The pizza dove in the oven" 
+                />
+
+                <Card
+                    img = "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80"
+                    alt = "Recipe cooked"
+                    title = "BBQ Chicken Pizza"
+                    description =  "Make the dove with all the ingredients. <br/>bake The pizza dove in the oven" 
+                />
+            </div>
         </div>
     );
 }
