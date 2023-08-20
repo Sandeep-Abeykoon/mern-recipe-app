f560f69ba5b07bc06a8520f91f072dba0580800f
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-20 11:50:36 +0530

    updated the code

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -4,6 +4,18 @@ export const CreateRecipe = () => {
     return (
         <div className="container">
             <h2 className="page-heading">Create Recipe</h2>
+
+            <form action="#">
+                <div className="card-details">
+                    <div className="card-box">
+                        <span className="details">Name</span>
+                        <input type="text" placeholder="Enter recipe name" />
+                    </div>
+                </div>
+                <div className="button">
+                    <button>Add Recipe</button>
+                </div>
+            </form>
         </div>
     );
 }
