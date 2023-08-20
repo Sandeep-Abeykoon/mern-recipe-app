2742c0853b21161613cacff8c54e51a0edcf66ff
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-20 13:08:53 +0530

    Update create-recipe.js

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -1,6 +1,22 @@
+import { useState } from 'react';
 import "./styles/create-recipe.css";
 
 export const CreateRecipe = () => {
+
+    const [recipe, setRecipe] = useState({
+        name: "",
+        ingredients: [],
+        instructions: "",
+        imageUrl: "",
+        cookingTime: 0,
+        userOwner: 0
+    });
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setRecipe({ ...recipe, [name]: value });
+    }
+
     return (
         <div className="container">
             <h2 className="page-heading">Create Recipe</h2>
@@ -9,12 +25,25 @@ export const CreateRecipe = () => {
                 <div className="card-details">
                     <div className="card-box">
                         <span className="details">Recipe Name</span>
-                        <input id="name" name="name" type="text" placeholder="Enter recipe name" />
+                        <input 
+                            id="name" 
+                            name="name" 
+                            type="text" 
+                            placeholder="Enter recipe name" 
+                            onChange={handleChange}
+                        />
                     </div>
 
                     <div className="card-box">
                         <span className="details">Description</span>
-                        <textarea name="description" id="description" cols="30" rows="10" placeholder="Enter the steps"></textarea>
+                        <textarea 
+                            name="description" 
+                            id="description" 
+                            cols="30" 
+                            rows="10" 
+                            placeholder="Enter the steps" 
+                            onChange={handleChange}>
+                        </textarea>
                     </div>
 
                     <div className="card-box">
@@ -23,12 +52,23 @@ export const CreateRecipe = () => {
 
                     <div className="card-box">
                         <span className="details">Image URL</span>
-                        <input id="image" name="image" type="text" placeholder="Copy paste the image url" />
+                        <input 
+                            id="imageUrl" 
+                            name="imageUrl" 
+                            type="text" 
+                            placeholder="Copy paste the image url" 
+                            onChange={handleChange}
+                        />
                     </div>
 
                     <div className="card-box">
                         <span className="details">Cooking Time (minutes)</span>
-                        <input id="cooking-time" name="cooking-time" type="text" placeholder="Enter cooking time in minutes" />
+                        <input 
+                            id="cookingTime" 
+                            name="cookingTime" 
+                            type="text" placeholder="Enter cooking time in minutes" 
+                            onChange={handleChange}
+                        />
                     </div>
                 </div>
                 <div className="button">
