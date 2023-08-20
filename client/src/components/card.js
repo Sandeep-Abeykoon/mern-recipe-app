3242ba7add37e7b83400a6141cb7436bff99cf05
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-20 20:52:12 +0530

    updated the code

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export const Card = () => {
+export const Card = (props) => {
   return (
     <div className='card'>
         <div className='card-body'>
             <img 
-                src="https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80" 
-                alt="Recipe cooked"
+                src= {props.img}
+                alt= {props.alt}
              />
-             <h2 className='card-title'>BBQ Chicken Pizza</h2>
-             <p className='class-description'></p>
+             <h2 className='card-title'>{props.title}</h2>
+             <p className='class-description'>{props.description}</p>
         </div>
         <button className='card-button'>View Recipe</button>
     </div>
